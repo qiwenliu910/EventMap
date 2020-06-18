2dafbbfafac0d1554a54f49722ba4e56e2cc4fee
Qiwen Liu <qiwen.liu@mail.utoronto.ca>
2020-06-18 15:48:55 -0400

    implemented maker functionality for the map - Qiwen Liu

--- csc309teamproject/src/components/Home/HomeMap.js
+++ csc309teamproject/src/components/Home/HomeMap.js
@@ -1,12 +1,45 @@
-import React from 'react';
-import { GoogleMap, withScriptjs, withGoogleMap } from "react-google-maps";
+import React, { useState } from "react";
+import { GoogleMap, withScriptjs, withGoogleMap, Marker, InfoWindow } from "react-google-maps";
+import * as crimeData from "../../data/crimeData.json"
 function Map() {
+
+  const [selectedCrime, setSelectedCrime] = useState(null);
+
   return (
     <GoogleMap
     defaultZoom={15}
     defaultCenter={{ lat:43.6629, lng: -79.3957}} // location of UofT
-    />
-
+    >
+    {crimeData.crimeList.map(crime => ( // List of all crimes
+      <Marker 
+        key={crime.properties.CRIME_ID} 
+        position={{
+          lat: crime.geometry.coordinates[1],
+          lng: crime.geometry.coordinates[0]
+        }}
+        onClick={() => {
+          setSelectedCrime(crime);
+        }}
+        />
+    ))}
+    {selectedCrime && ( //if a crime is selected
+        <InfoWindow
+          position={{
+            lat: selectedCrime.geometry.coordinates[1],
+            lng: selectedCrime.geometry.coordinates[0]
+          }}
+          onCloseClick={() => {
+            setSelectedCrime(null);
+          }}
+        >
+          <div> //display
+            <h2>{selectedCrime.properties.TITLE}</h2>
+            <p> posted by: {selectedCrime.properties.ARTHOR}</p>
+            <h4>{selectedCrime.properties.DESCRIPTION}</h4>
+          </div>
+        </InfoWindow>
+      )}
+  </GoogleMap>
   );
 }
 
