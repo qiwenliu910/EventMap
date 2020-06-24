4f2f4b1079c7b7b06295e81f1d488ca6f2ce9e40
KenSB <kennethsb.wong@gmail.com>
2020-06-24 18:34:22 -0400

    Added new account components - Kenneth Wong

--- csc309teamproject/src/components/view/Account.js
+++ csc309teamproject/src/components/view/Account.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import { Container } from 'react-bootstrap'
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import AccountPage from '../Account/AccountPage'
+import AccountEvents from '../Account/AccountEvents'
+import AccountSettings from '../Account/AccountSettings'
+
 class Account extends React.Component {
   constructor(props) {
     super(props);
   }
   render() {
     return (
-      <AccountPage state={this.props.state}/>
+      <Switch> { /* Similar to a switch statement - shows the component depending on the URL path */ }
+        { /* Each Route below shows a different component depending on the exact path in the URL  */ }
+        <Route exact path='/account/events' render={() =>
+                        (<AccountEvents state={this.props.state}/>)}/>
+        <Route exact path='/account/info' render={() =>
+                        (<AccountPage state={this.props.state}/>)}/>
+        <Route exact path='/account/settings' render={() =>
+                        (<AccountSettings state={this.props.state}/>)}/>
+      </Switch>
     );
   }
 }
